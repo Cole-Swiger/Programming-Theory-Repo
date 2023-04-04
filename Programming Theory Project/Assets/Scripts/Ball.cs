7e2762573bce532a7eff32a4f83e4e25c6c7fa0d
Cole Swiger <jcole598@aol.com>
2023-04-04 18:11:40 -0400

    Basic Functionality: Added objects and baseline scripts to begin implementing OOP principles.

---
+++ Programming Theory Project/Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Inheritance
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void Roll()
+    {
+
+    }
+
+    protected void Roll(float force)
+    {
+
+    }
+
+    protected void Bounce()
+    {
+        Debug.Log(gameObject.name + " is bouncing");
+    }
+
+    protected void Bounce(float force)
+    {
+
+    }
+}
